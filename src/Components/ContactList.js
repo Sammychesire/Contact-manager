ff2612b86954aee659a6b482b0b080f7af4b09eb
bob <sammy.chesire@student.moringaschool.com>
2022-10-05 17:37:37 +0300

    create components

---
+++ src/Components/ContactList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ContactList(){
+    return (
+        <React.Fragment>
+            <h2>ContactList</h2>
+        </React.Fragment>
+    )
+}
+
+export default ContactList;
---
+++ src/Components/EditContact.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EditContact(){
+    return (
+        <React.Fragment>
+            <h2>EditContact</h2>
+        </React.Fragment>
+    )
+}
+
+export default EditContact;
