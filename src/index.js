802dbeb9e8d00fd28093f003bbe06fa7c362a845
bob <sammy.chesire@student.moringaschool.com>
2022-10-05 22:40:18 +0300

    import browser router

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter> 
   </React.StrictMode>
 );
